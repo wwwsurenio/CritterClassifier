8820cc25c701163df1077ac37bd6411384d79864
Suren <105368039+wwwsurenio@users.noreply.github.com>
2023-03-11 15:09:02 +0100

    Added all UI elements and chose color palette for the app

--- CritterClassifier/ViewController.swift
+++ CritterClassifier/ViewController.swift
@@ -7,12 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(red: 0.92, green: 0.11, blue: 0.21, alpha: 1.00)]
+        
     }
+
 
 
 }
