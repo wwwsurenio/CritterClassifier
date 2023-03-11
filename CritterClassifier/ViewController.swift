1a58c2c3cd099653cbdb0b7ece1a09bf6f537677
Suren <105368039+wwwsurenio@users.noreply.github.com>
2023-03-11 14:22:52 +0100

    Initial Commit

---
+++ CritterClassifier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CritterClassifier
+//
+//  Created by User01 on 11/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
