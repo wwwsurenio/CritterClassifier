2560dd5926216c08ba4ba2b0cec134f5b940ce0f
Suren <105368039+wwwsurenio@users.noreply.github.com>
2023-03-11 16:57:33 +0100

    Changing from if else to a switch statement

--- CritterClassifier/ViewController.swift
+++ CritterClassifier/ViewController.swift
@@ -54,29 +54,52 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 fatalError("Failed to process image! ")
             }
             
-            if let firstResult = results.first{
-                if firstResult.identifier.contains("Dog"){
+//            if let firstResult = results.first{
+//                if firstResult.identifier.contains("Dog"){
+//                    self.imageView.image = UIImage(named: "dog")
+//                    self.navigationItem.title = "It looks like a dog!"
+//                    self.textBox.text = "This looks like a Dog, our classifier is confident with a \(String(format: "%.0f", (firstResult.confidence * 100))) % "
+//                    self.textBox.sizeToFit()
+//                }else if firstResult.identifier.contains("Cat"){
+//                    self.imageView.image = UIImage(named: "cat")
+//                    self.navigationItem.title = "It looks like a cat!"
+//                    self.textBox.text = "This looks like a Cat, our classifier is confident with a \(String(format: "%.0f", (firstResult.confidence * 100))) % "
+//                    self.textBox.sizeToFit()
+//                }else if firstResult.identifier.contains("Rabbit"){
+//                    self.imageView.image = UIImage(named: "rabbit")
+//                    self.navigationItem.title = "It looks like a Rabbit!"
+//                    self.textBox.text = "This looks like a Rabbit, our classifier is confident with a \(String(format: "%.0f", (firstResult.confidence * 100))) % "
+//                    self.textBox.sizeToFit()
+//                }else{
+//                    self.navigationItem.title = "Not a Critter !"
+//                }
+//            }
+            
+            
+            if let firstResult = results.first {
+                switch firstResult.identifier {
+                case let id where id.contains("Dog"):
                     self.imageView.image = UIImage(named: "dog")
                     self.navigationItem.title = "It looks like a dog!"
-                    self.textBox.text = "This looks like a Dog, our classifier is confident with \(String(format: "%.0f", (firstResult.confidence * 100))) % "
+                    self.textBox.text = "This looks like a Dog, our classifier is confident with a \(String(format: "%.0f", (firstResult.confidence * 100))) % "
                     self.textBox.sizeToFit()
-                }else if firstResult.identifier.contains("Cat"){
+                case let id where id.contains("Cat"):
                     self.imageView.image = UIImage(named: "cat")
                     self.navigationItem.title = "It looks like a cat!"
-                    self.textBox.text = "This looks like a Cat, our classifier is confident with \(String(format: "%.0f", (firstResult.confidence * 100))) % "
+                    self.textBox.text = "This looks like a Cat, our classifier is confident with a \(String(format: "%.0f", (firstResult.confidence * 100))) % "
                     self.textBox.sizeToFit()
-                }else if firstResult.identifier.contains("Rabbit"){
+                case let id where id.contains("Rabbit"):
                     self.imageView.image = UIImage(named: "rabbit")
                     self.navigationItem.title = "It looks like a Rabbit!"
-                    self.textBox.text = "This looks like a Rabbit, our classifier is confident with \(String(format: "%.0f", (firstResult.confidence * 100))) % "
+                    self.textBox.text = "This looks like a Rabbit, our classifier is confident with a \(String(format: "%.0f", (firstResult.confidence * 100))) % "
                     self.textBox.sizeToFit()
-                }else{
+                default:
                     self.navigationItem.title = "Not a Critter !"
                 }
             }
             
-            print("These are the results we get from the results constant\(results) END of first print")
-            print("This is the 2nd Print, confidence of first is equal to \(results.first?.confidence)")
+//            print("These are the results we get from the results constant\(results) END of first print")
+//            print("This is the 2nd Print, confidence of first is equal to \(results.first?.confidence)")
         }
         
         let handler = VNImageRequestHandler(ciImage: image)
